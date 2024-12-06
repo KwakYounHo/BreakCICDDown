b97c80ef1f9060baa1bc00f73e46c076d23dc219
곽윤호 <kwakdm95@gmail.com>
2024-12-06 16:26:25 +0900

    Create-Invalidation 적용 실험

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,8 @@ if (root) {
   const p1 = document.createElement("p");
   p1.innerHTML = sayHellp;
   root.appendChild(p1);
+
+  const p2 = document.createElement("p");
+  p2.innerHTML = "Create Invalidation을 적용 테스트를 해보겠습니다.";
+  root.appendChild(p2);
 }
